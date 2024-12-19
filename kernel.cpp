0f068a2b8b5cf807bf2a39208d46e152d5d4ade0
ac-manjaro <2419280542@qq.com>
2024-12-19 19:23:11 +0800

    adjust .gitignore and adjust the printf

--- kernel.cpp
+++ kernel.cpp
@@ -2,12 +2,52 @@
 #include "gdt.h"
 #include "types.h"
 
+void flushScreen()
+{
+    uint16_t *VideoMemory = (uint16_t *)0xb8000;
+    for (int i = 0; i < 80 * 25; i++)
+    {
+        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | ' ';
+    }
+}
+
 void printf(const char *str)
 {
-    static unsigned short *VideoMemory = (unsigned short *)0xb8000;
+    static uint16_t *VideoMemory = (uint16_t *)0xb8000;
+    static uint8_t x = 0, y = 0;
 
     for (int i = 0; str[i] != '\0'; ++i)
-        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+    {
+        switch (str[i])
+        {
+        case '\n':
+            x = 0;
+            y++;
+            break;
+        default:
+            VideoMemory[80 * y + x] = (VideoMemory[80 * y + x] & 0xFF00) | str[i];
+            x++;
+            break;
+        }
+        // 80 characters wide, 25 lines tall
+        if (x >= 80)
+        {
+            x = 0;
+            y++;
+        }
+        if (y >= 25)
+        {
+            for (y = 0; y < 25; y++)
+            {
+                for (x = 0; x < 80; x++)
+                {
+                    VideoMemory[80 * y + x] = (VideoMemory[80 * y + x] & 0xFF00) | ' ';
+                }
+            }
+            x = 0;
+            y = 0;
+        }
+    }
 }
 
 typedef void (*constructor)();
@@ -21,11 +61,16 @@ extern "C" void callConstructors()
 
 extern "C" void kernelMain(const void *multiboot_structure, unsigned int /*multiboot_magic*/)
 {
-    const char *welcome = "Hello World! --- http://www.AlgorithMan.de";
-
-    printf(welcome);
-
     GlobalDescriptorTable gdt;
+
+    flushScreen();
+    const char *welcome = "Hello World!\n";
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf(welcome);
+    }
+
     while (1)
         ;
 }
