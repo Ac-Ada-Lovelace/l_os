2c843c01cc7c4c3c6cf5a37e973a708dbc4f3892
ac-manjaro <2419280542@qq.com>
2024-12-21 01:41:22 +0800

    def some int

--- interrupts.h
+++ interrupts.h
@@ -1,14 +1,31 @@
 #ifndef __INTERRUPTS_H
 #define __INTERRUPTS_H
 
+#include "gdt.h"
 #include "port.h"
 #include "types.h"
+class InterruptManager {
+protected:
+  struct GateDescriptor {
+    uint16_t handlerAddressLowBits;
+    uint16_t gdt_codeSegmentSelector;
+    uint8_t reserved;
+    uint8_t access;
+    uint16_t handlerAddressHighBits;
+  } __attribute__((packed));
 
-class InterruptManager
-{
-  public:
-    static uint32_t handleInterrupt(uint8_t interruptNumber, uint32_t esp);
-    static void HandleInterruptRequest();
+  static GateDescriptor interruptDescriptorTable[256];
+
+  static void SetInterruptDescriptorTableEntry(
+      uint8_t interruptNumber, uint16_t codeSegmentSelectorOffset,
+      void (*handler)(), uint8_t DescriptorPrivilegeLevel,
+      uint8_t DescriptorType);
+
+public:
+  static uint32_t handleInterrupt(uint8_t interruptNumber, uint32_t esp);
+
+  static void HandleInterruptRequest0x00();
+  static void HandleInterruptRequest0x01();
 };
 
 #endif
